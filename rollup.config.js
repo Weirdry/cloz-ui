ec3d90aadcd3e56cc8c46df3f8b2fc9a2349ddee
Drawdl <37974622+Drawdl@users.noreply.github.com>
2022-07-13 04:38:13 +0900

    Try to solve styled components problem (failed-2)

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,6 @@
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
-import babel from '@rollup/plugin-babel';
 import dts from "rollup-plugin-dts";
 
 import { terser } from "rollup-plugin-terser";
