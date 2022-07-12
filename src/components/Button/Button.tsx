bb4a5cd0792aae3ffbbb152fa08faadb714b8628
weirdry <granrr.z@gmail.com>
2022-07-12 17:43:52 +0900

    Dev set up

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,11 +1,22 @@
 import React from "react";
+import styled from "styled-components"
 
 export interface ButtonProps {
   label: string;
 }
 
 const Button = (props: ButtonProps) => {
-  return <button>{props.label}</button>;
+  return <Container>{props.label}</Container>;
 };
+
+export const Container = styled.div`
+  display: flex;
+  width: auto;
+  height: auto;
+  padding: 0.5rem;
+  background-color: #000000;
+  color: #ffffff;
+  font-size: 2rem;
+`
 
 export default Button;
