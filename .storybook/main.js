f9248af4e86f6c1ccfb7a311854898c8d2f07c37
ED <ed@nemomarket.kr>
2022-07-30 00:13:04 +0900

    Fixed storybook addon-docs

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,7 +4,7 @@ const toPath = (_path) => path.join(process.cwd(), _path)
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
-    '@storybook/addon-docs/preset',
+    '@storybook/addon-docs',
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
